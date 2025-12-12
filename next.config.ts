5b09dc4b109eb9ad479e0fba13111a02e22fe069
Miran Setinc <miran.setinc@gmail.com>
2025-12-12 08:24:53 +0100

    next config changed to function, to be able to access env var in docker

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,23 @@
 import type { NextConfig } from 'next'
 
-const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL
+const nextConfig = (
+  phase: string,
+  { defaultConfig }: { defaultConfig: NextConfig }
+): NextConfig => {
+  const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      new URL('https://assets.untappd.com/social/**'),
-      new URL(`${NEXT_PUBLIC_API_URL}/api/beer-labels/**`),
-      new URL(`${NEXT_PUBLIC_API_URL}/api/beer-photos/**`)
-    ],
-    dangerouslyAllowSVG: true
-  },
-  typescript: {
-    // TODO: remove ignore when preparing for production
-    ignoreBuildErrors: true
+  return {
+    images: {
+      remotePatterns: [
+        new URL('https://assets.untappd.com/social/**'),
+        new URL(`${NEXT_PUBLIC_API_URL}/api/beer-labels/**`),
+        new URL(`${NEXT_PUBLIC_API_URL}/api/beer-photos/**`)
+      ],
+      dangerouslyAllowSVG: true
+    },
+    typescript: {
+      ignoreBuildErrors: true
+    }
   }
 }
 
